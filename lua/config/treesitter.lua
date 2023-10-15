3b8177c00bb415f47f8b1aa86972d992cfb2b528
Ivan Polanis <polanisivan@gmail.com>
2023-10-15 10:07:50 -0300

    fixing parsers

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,11 +1,11 @@
 require("nvim-treesitter.configs").setup {
-ensure_installed = {"c","java","python","lua","c++","javascript","vim","vimdoc","latex","rust"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {}, -- List of parsers to ignore installing
+    ensure_installed = { "c", "java", "python", "lua", "bash", "javascript", "vim", "vimdoc", "latex", "rust" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = {},                                                                                         -- List of parsers to ignore installing
     sync_install = false,
     auto_install = true,
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {}, -- list of language that will be disabled
+        disable = {},  -- list of language that will be disabled
         additional_vim_regex_highlighting = true,
     },
     context_commentstring = {
