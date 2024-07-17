42f059a8490f91f3fdfad33cdda148f84cffd20c
Ivan Polanis <polanisivan@gmail.com>
2024-07-17 18:13:31 -0300

    Cleaning and optimizing config - Solved 10.X problems

--- init.lua
+++ init.lua
@@ -3,5 +3,3 @@ require('plugins')  -- lua/plugins.lua
 require('autocmd')  -- lua/autocmd.lua
 require('mappings') -- lua/mappings.lua
 require('options')  -- lua/options.lua
-
-vim.cmd [[colorscheme catppuccin-mocha]]
--- minimal.lua
+++ minimal.lua
@@ -1,9 +1,9 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd[[packadd packer.nvim]]
 
 local packer = require('packer')
 packer.init {
-    enable = true,
-    threshold = 0,
+	enable = true,
+	threshold = 0,
 }
 
 local use = packer.use
@@ -11,18 +11,3 @@ packer.reset()
 
 packer.startup({function()
 end})
-
--- put plugin config here
-
-local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
